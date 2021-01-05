61064064946cf8decc94bcbfe4f390029659f9e0
Simon Charters <simon259@hotmail.co.uk>
2021-01-05 17:35:26 +0000

    episode 6 done

--- who_done_it.js
+++ who_done_it.js
@@ -35,15 +35,82 @@ episode = 2
 
 epsode = 3
 
-let murderer = 'Professor Plum';
+// let murderer = 'Professor Plum';
 
-const declareMurderer = function() {
-    let murderer = 'Mrs. Peacock';
-    return `The murderer is ${murderer}.`;
+// const declareMurderer = function() {
+//     let murderer = 'Mrs. Peacock';
+//     return `The murderer is ${murderer}.`;
+// }
+
+// const firstVerdict = declareMurderer();
+// console.log('First Verdict: ', firstVerdict);
+
+// const secondVerdict = `The murderer is ${murderer}.`;
+// console.log('Second Verdict: ', secondVerdict);
+
+// First Verdict is Mrs. Peacock. the murderer is changed and them printed out within the function
+// Second Verdict is Professor Plum. the declareMurderer function only changes the murderer vatiable inside of the block so when murderer is called again it reverts to Professor Plum
+
+episode = 4
+
+// let suspectOne = 'Miss Scarlet';
+// let suspectTwo = 'Professor Plum';
+// let suspectThree = 'Mrs. Peacock';
+
+// const declareAllSuspects = function() {
+//     let suspectThree = 'Colonel Mustard';
+//     return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+// }
+
+// const suspects = declareAllSuspects();
+// console.log(suspects);
+// console.log(`Suspect three is ${suspectThree}.`);
+
+// suspects should be [scarlet, plum, mustard] as suspects was assigned using the declareAllSuspects functions and suspectThree was changed inside the function
+// suspectThree should be Mrs. Peacock as it does not use the function where it changes suspectThree
+
+episode = 5
+
+// const scenario = {
+//     murderer: 'Miss Scarlet',
+//     room: 'Kitchen',
+//     weapon: 'Candle Stick'
+//   };
+  
+// const changeWeapon = function(newWeapon) {
+//     scenario.weapon = newWeapon;
+//   }
+  
+// const declareWeapon = function() {
+//     return `The weapon is the ${scenario.weapon}.`;
+//   }
+  
+// changeWeapon('Revolver');
+// const verdict = declareWeapon();
+// console.log(verdict);
+
+// Revoler. the weapon value of scanrio is changed to Revolver using the changeWeapon function
+
+episode = 6
+
+let murderer = 'Colonel Mustard';
+
+const changeMurderer = function() {
+  murderer = 'Mr. Green';
+
+  const plotTwist = function() {
+    murderer = 'Mrs. White';
+  }
+
+  plotTwist();
 }
 
-const firstVerdict = declareMurderer();
-console.log('First Verdict: ', firstVerdict);
+const declareMurderer = function () {
+  return `The murderer is ${murderer}.`;
+}
 
-const secondVerdict = `The murderer is ${murderer}.`;
-console.log('Second Verdict: ', secondVerdict);
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+
+// Mrs. White
